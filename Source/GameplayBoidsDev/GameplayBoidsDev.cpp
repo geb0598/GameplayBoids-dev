60e29256c37a1fc70e2da589fb5d74a084163950
geb0598 <89014972+geb0598@users.noreply.github.com>
2026-06-13 15:38:16 +0900

    chore: initial project scaffold and git setup

---
+++ Source/GameplayBoidsDev/GameplayBoidsDev.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameplayBoidsDev.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameplayBoidsDev, "GameplayBoidsDev" );
